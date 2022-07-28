d3eb2312050cf7d38e26127980e19e785f7eed8f
Abdul Qayyum <abdulqayyum3810@gmail.com>
2022-07-28 11:39:24 -0700

    Project files

---
+++ AspNetProject1/Login.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="AspNetProject1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
+    <title></title>
+</head>
+<body>
+    <div class="  container h-100 w-50 p-3" id="outerContainer">
+        <h1 class="display-4 mb-4">Login</h1>
+        <form id="loginFrom" class="border border-info p-5 rounded bg-light">
+
+
+            <div class="alert alert-danger d-none text-center w-75 m-auto" role="alert" id="loginError">
+                Username or password incorrect
+            </div>
+
+
+
+            <div class="form-group ">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required class="form-control" runat="server">
+
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required class="form-control" runat="server">
+            </div>
+
+            <div class="form-group">
+                <label for="type">User Type</label>
+                <select name="type" id="type" class="custom-select" required runat="server">
+                    <option value="select" selected disabled>select</option>
+                    <option value="Admin">Admin</option>
+                    <option value="Accountant">Accountant</option>
+                </select>
+                <div class="invalid-feedback">
+                    Please provide user type.
+                </div>
+            </div>
+            <input type="submit" name="loginButton" id="loginButton" value="Login"
+                class="btn btn-success btn-block w-25 m-auto">
+        </form>
+
+    </div>
+
+    <script>
+        //$("#loginButton").on("click", () => {
+        //    $("#loginError").removeClass("d-none")
+        //    $("#type").addClass("is-invalid")
+
+        //})
+
+    </script>
+</body>
+</html>
---
+++ AspNetProject1/User.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AspNetProject1
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Email { get; set; }
+
+
+    }
+
+    class Account : User
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product(){Id=198,Name="oil",Price=100}
+        };
+
+        private static List<Customer> customers = new List<Customer>()
+        {
+            new Customer(){Id=102,Name="Abdullah",Email="abdullah@gmail.com"}
+        };
+
+        public static void AddProduct(string name, double price)
+
+        {
+            Random rand = new Random();
+            string idstr = $"{products.Count}{rand.Next(10, 100)}";
+            int id = Convert.ToInt32(idstr);
+            products.Add(new Product() { Id = id, Name = name, Price = price });
+        }
+        public static void UpdateProduct(int id,string name, double price)
+        {
+            products.Where(p => p.Id==id).ToList()
+           .ForEach(x => {
+               x.Name = name;
+               x.Price = price;
+                           });
+        }
+
+        public static void DeleteProduct(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+
+        
+        public static void AddCustomer(string name, string email)
+        {
+            Random rand = new Random();
+            string idstr = $"{customers.Count}{rand.Next(10, 100)}";
+            int id = Convert.ToInt32(idstr);
+            customers.Add(new Customer() { Id = id, Name = name, Email = email });
+        }
+        public static void UpdateCustomer(int id, string name, string email)
+        {
+            customers.Where(c => c.Id == id).ToList()
+           .ForEach(x => {
+               x.Name = name;
+               x.Email = email;
+           });
+            
+        }
+        public static void DeleteCustomer(int id)
+        {
+            customers.RemoveAll(c => c.Id == id);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+
+
+
+    }
+    class Admin : Account
+    {
+        private static List<Accountant> accountants = new List<Accountant>()
+        {
+            new Accountant(){Id=122,Name="Amjad",Email="amjad@gmail.com"}
+        };
+
+
+        public static void AddAccountant(string name, string email)
+        {
+            Random rand = new Random();
+            string idstr = $"{accountants.Count}{rand.Next(10, 100)}";
+            int id = Convert.ToInt32(idstr);
+            accountants.Add(new Accountant() { Id = id, Name = name, Email = email });
+        }
+
+        public static void UpdateAccountant(int id, string name, string email)
+        {
+            accountants.Where(a => a.Id == id).ToList()
+           .ForEach(x => {
+               x.Name = name;
+               x.Email = email;
+           });
+
+        }
+        public static void DeleteAccountant(int id)
+        {
+            accountants.RemoveAll(a => a.Id == id);
+        }
+    }
+    class Accountant : Account
+    {
+
+    }
+    class Customer : User
+    {
+  
+    }
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+    class BillEntries
+    {
+
+    }
+}
